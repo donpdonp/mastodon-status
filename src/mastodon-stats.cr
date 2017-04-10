00c423b6c731a59b6d4c4569399851bdfc68846b
Don Park <donp@donp.org>
2017-04-10 12:56:47 -0700

    load and scrape

--- src/mastodon-stats.cr
+++ src/mastodon-stats.cr
@@ -1,6 +1,42 @@
 require "./mastodon-stats/*"
 require "http/client"
+require "xml"
 
 module Mastodon::Stats
-  response = HTTP::Client.get "http://www.example.com"
+  class Scrape
+    def go
+      response = HTTP::Client.get "https://instances.mastodon.xyz"
+      document = XML.parse(response.body)
+      nodes = document.xpath_nodes("//tbody/tr")
+      nodes.each do |node|
+        xtoj(node)
+      end
+    end
+
+    def xtoj(node)
+      puts node
+      host = node.xpath_node("td[@scope='row']")
+      if host
+        puts "host: #{host["data-value"]}"
+      end
+      host = node.xpath_node("td[@class='table-danger']")
+      if host
+        puts "up: #{host.content}"
+      end
+      host = node.xpath_node("td[3]")
+      if host
+        puts "c: #{host.content}"
+      end
+      host = node.xpath_node("td[4]")
+      if host
+        puts "c: #{host.content}"
+      end
+      host = node.xpath_node("td[5]")
+      if host
+        puts "c: #{host.content}"
+      end
+    end
+  end
 end
+
+Mastodon::Stats::Scrape.new.go
