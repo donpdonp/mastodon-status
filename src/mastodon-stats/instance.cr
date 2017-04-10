1eb6ba7eb747e20bc1b39d43960b7d5db76090bc
Don Park <donp@donp.org>
2017-04-10 13:43:33 -0700

    summarize

--- src/mastodon-stats/instance.cr
+++ src/mastodon-stats/instance.cr
@@ -1,10 +1,49 @@
 module Mastodon::Stats
   class Instance
     @name = ""
-    property name
+    @up = false
+    @users = -1
+    @statuses = -1
+    @connections = -1
+    property name, up, users, statuses, connections
+
+    def initialize(@xml : XML::Node)
+      from_xml(xml)
+    end
 
     def to_json
-      "json here"
+      JSON.build do |json|
+        json.object do
+          json.field "name", @name
+          json.field "up", @up
+          json.field "users", @users
+          json.field "statuses", @statuses
+          json.field "connections", @connections
+        end
+      end
+    end
+
+    def from_xml(node)
+      host = node.xpath_node("td[1]")
+      if host
+        self.up = host.content == "UP"
+      end
+      host = node.xpath_node("td[2]")
+      if host
+        self.name = host["data-value"]
+      end
+      host = node.xpath_node("td[3]")
+      if host && host.content && host.content.size > 0
+        self.users = host.content.to_i
+      end
+      host = node.xpath_node("td[4]")
+      if host && host.content && host.content.size > 0
+        self.statuses = host.content.to_i
+      end
+      host = node.xpath_node("td[5]")
+      if host && host.content && host.content.size > 0
+        self.connections = host.content.to_i
+      end
     end
   end
 end
--- src/mastodon-stats/scrape.cr
+++ src/mastodon-stats/scrape.cr
@@ -5,39 +5,16 @@ require "json"
 module Mastodon::Stats
   class Scrape
     def go
-      response = HTTP::Client.get "https://instances.mastodon.xyz"
+      url = "https://instances.mastodon.xyz"
+      response = HTTP::Client.get url
       document = XML.parse(response.body)
       nodes = document.xpath_nodes("//tbody/tr")
-      nodes.each do |node|
-        puts xtoj(node).to_json
-      end
-    end
-
-    def xtoj(node)
-      z = Instance.new
-      puts node
-      host = node.xpath_node("td[@scope='row']")
-      if host
-        z.name = "a"
-        puts "host: #{host["data-value"]}"
-      end
-      host = node.xpath_node("td[@class='table-danger']")
-      if host
-        puts "up: #{host.content}"
-      end
-      host = node.xpath_node("td[3]")
-      if host
-        puts "c: #{host.content}"
-      end
-      host = node.xpath_node("td[4]")
-      if host
-        puts "c: #{host.content}"
-      end
-      host = node.xpath_node("td[5]")
-      if host
-        puts "c: #{host.content}"
-      end
-      z
+      instances = [] of Instance
+      nodes.map { |node| instances.push(Instance.new(node)) }
+      puts "#{instances.size} instances. " +
+           "#{instances.sum { |i| i.users }} users. " +
+           "#{instances.sum { |i| i.statuses }} statuses. " +
+           "#{instances.sum { |i| i.connections }} connections."
     end
   end
 end
