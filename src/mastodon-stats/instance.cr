54d6af5a9fbe685d56f4547d8012506666096181
Don Park <donp@donp.org>
2017-05-01 10:35:44 -0700

    use the JSON, Luke.

--- src/mastodon-stats/instance.cr
+++ src/mastodon-stats/instance.cr
@@ -1,14 +1,29 @@
+require "json"
+
 module Mastodon::Stats
   class Instance
-    @name = ""
-    @up = false
-    @users = -1
-    @statuses = -1
-    @connections = -1
+    @name = nil
+    @uptime = nil
+    @up = nil
+    @ipv6 = nil
+    @openRegistrations = nil
+    @users = nil
+    @statuses = nil
+    @connections = nil
     property name, up, users, statuses, connections
 
-    def initialize(@xml : XML::Node)
-      from_xml(xml)
+    JSON.mapping(
+      name: String?,
+      uptime: Float64?,
+      up: Bool?,
+      ipv6: Bool?,
+      openRegistrations: Bool?,
+      users: Int64?,
+      statuses: Int64?,
+      connections: Int64?
+    )
+
+    def initialize
     end
 
     def to_json
@@ -20,29 +35,6 @@ module Mastodon::Stats
           json.field "statuses", @statuses
           json.field "connections", @connections
         end
-      end
-    end
-
-    def from_xml(node)
-      host = node.xpath_node("td[1]")
-      if host
-        self.up = host.content == "UP"
-      end
-      host = node.xpath_node("td[3]")
-      if host
-        self.name = host["data-value"]
-      end
-      host = node.xpath_node("td[4]")
-      if host && host.content && host.content.size > 0
-        self.users = host.content.to_i
-      end
-      host = node.xpath_node("td[5]")
-      if host && host.content && host.content.size > 0
-        self.statuses = host.content.to_i
-      end
-      host = node.xpath_node("td[6]")
-      if host && host.content && host.content.size > 0
-        self.connections = host.content.to_i
       end
     end
   end
--- src/mastodon-stats/scrape.cr
+++ src/mastodon-stats/scrape.cr
@@ -5,24 +5,24 @@ require "json"
 module Mastodon::Stats
   class Scrape
     def go
-      url = "https://instances.mastodon.xyz"
+      url = "https://instances.mastodon.xyz/instances.json"
       response = HTTP::Client.get url
-      document = XML.parse(response.body)
-      nodes = document.xpath_nodes("//tbody/tr")
-      instances = [] of Instance
-      nodes.map { |node| instances.push(Instance.new(node)) }
+      instances = Array(Instance).from_json(response.body)
       instances_up = instances.reject { |i| !i.up }.size
-      instances_users = instances.sum { |i| i.users }
-      instances_statuses = instances.sum { |i| i.statuses }
-      instances_connections = instances.sum { |i| i.connections }
+      instances_open = instances.reject { |i| !i.openRegistrations }.size
+      instances_users = instances.sum { |i| i.users || 0 }
+      instances_statuses = instances.sum { |i| i.statuses || 0 }
+      instances_connections = instances.sum { |i| i.connections || 0 }
       puts "#{instances.size} instances. " +
            "#{instances_up} up. " +
+           "#{instances_open} open. " +
            "#{instances_users} users. " +
            "#{instances_statuses} statuses. " +
            "#{instances_connections} connections."
       stat_url = "http://localhost:8086/write?db=mastodon"
       HTTP::Client.post(stat_url, nil, "instances_count value=#{instances.size}")
       HTTP::Client.post(stat_url, nil, "instances_up value=#{instances_up}")
+      HTTP::Client.post(stat_url, nil, "instances_open value=#{instances_open}")
       HTTP::Client.post(stat_url, nil, "instances_users value=#{instances_users}")
       HTTP::Client.post(stat_url, nil, "instances_statuses value=#{instances_statuses}")
       HTTP::Client.post(stat_url, nil, "instances_connections value=#{instances_connections}")
